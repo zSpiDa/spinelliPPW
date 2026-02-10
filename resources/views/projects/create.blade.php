1558a14c2c5d1d96b6a0f2a2159bbb6ac76923b6
spinelli <spinelli441@gmail.com>
2026-02-10 15:20:48 +0100

    modifica create.blade.php

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,12 +3,20 @@
 @section('content')
     <h1>Crea nuovo progetto</h1>
 
-    <form action="{{ route('projects.store') }}" method="POST">
+    <form action="{{ route('projects.store') }}" enctype="multipart/form-data" method="POST">
         @csrf
 
         <div>
-            <label>Titolo</label>
+            <label>Titolo:</label>
             <input type="text" name="title" required>
+        </div>
+        <div>
+            <label>Descrizione:</label>
+            <textarea name="description"></textarea>
+        </div>
+        <div>
+            <label>Allega documento (PDF):</label>
+            <input type="file" name="file" accept=".pdf">
         </div>
 
         <div>
